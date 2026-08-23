69b4b581d0101f3039f4f96660dc70ab8b5994b0
loydmawira-cyber <loydmawira@gmail.com>
2026-08-23 19:35:11 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -6,14 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.ui.MkulimaApp
 import com.example.ui.theme.MkulimaTheme
+import com.google.firebase.FirebaseApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (FirebaseApp.getApps(this).isEmpty()) {
+            FirebaseApp.initializeApp(this)
+        }
         enableEdgeToEdge()
         setContent {
             MkulimaApp()
         }
     }
 }
-

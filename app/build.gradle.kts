7a9f91ce63308e7e58c8f8caa60661ebd8b057d5
loydmawira-cyber <loydmawira@gmail.com>
2026-09-03 10:52:02 +0300

    Increment versionCode from 2 to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     applicationId = "com.aistudio.mkulimafarm.xrqz"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
+    versionCode = 3
     versionName = "1.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

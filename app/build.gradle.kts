ef1cb374a597d8d30c0082fe9be839ec76180923
loydmawira-cyber <loydmawira@gmail.com>
2026-09-10 11:50:04 +0300

    Bump version code to 9 and version name to 1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.mkulimafarm.xrqz"
     minSdk = 24
     targetSdk = 36
-    versionCode = 8
-    versionName = "1.8"
+    versionCode = 9
+    versionName = "1.9"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }

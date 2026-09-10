774c1ca7a2d97971628e5d303f8ef96289e45be6
loydmawira-cyber <loydmawira@gmail.com>
2026-09-10 23:36:06 +0300

    Update version code and version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.mkulimafarm.xrqz"
     minSdk = 24
     targetSdk = 36
-    versionCode = 9.1
-    versionName = "1.9"
+    versionCode = 10
+    versionName = "2.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
